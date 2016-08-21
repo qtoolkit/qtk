19454f00019ed8b92801c36686576cfefb694c33
jim <xianjimli@hotmail.com>
2016-08-21 10:41:46 +0800

    add emitter

---
+++ src/emitter.ts
@@ -0,0 +1,21 @@
+/// <reference path="../typings/globals/eventemitter3/index.d.ts"/>
+
+import EventEmitter = require("eventemitter3");
+
+export = class Emitter extends EventEmitter {
+	constructor() {
+		super();
+	}
+
+	public addEventListener(type, callback) {
+		this.addListener(type, callback);
+	}
+	
+	public removeEventListener(type, callback) {
+		this.removeListener(type, callback);
+	}
+
+	public dispatchEvent(evt) {
+		this.emit(evt.type, evt);
+	}
+}
