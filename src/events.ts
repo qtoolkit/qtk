bfa4a9ab06af908456f349daff3c7417945d72b7
jim <xianjimli@hotmail.com>
2016-08-22 10:37:46 +0800

    add

---
+++ src/events.ts
@@ -0,0 +1,13 @@
+
+export const KEYUP = "qtk-keyup";
+export const WHEEL = "qtk-wheel";
+export const KEYDOWN = "qtk-keydown";
+export const CONTEXT_MENU = "qtk-context-menu";
+export const POINTER_DOWN = "qtk-pointer-down";
+export const POINTER_MOVE = "qtk-pointer-move";
+export const POINTER_UP   = "qtk-pointer-up";
+export const POINTER_OUT  = "qtk-pointer-out";
+export const POINTER_OVER = "qtk-pointer-over";
+export const CHANGED = "changed"
+export const MOVE = "move";
+export const RESIZE = "resize";
