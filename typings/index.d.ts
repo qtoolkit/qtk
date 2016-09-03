dc7f7e02ca394b746eb78920cce85f735229cbc6
jim <xianjimli@hotmail.com>
2016-09-03 09:48:05 +0800

    add scroller

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/es6-promise/index.d.ts" />
 /// <reference path="globals/eventemitter3/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/scroller/index.d.ts" />
 /// <reference path="globals/tween.js/index.d.ts" />
 /// <reference path="globals/whatwg-fetch/index.d.ts" />
